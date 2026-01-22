0e61e21f03b2bb5de9218dfdf6e0166abc2b1219
Marina Gurgel <marina@yourdigitalist.com>
2026-01-22 10:24:19 +0000

    Email invoice functionality explained

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,23 +1,23 @@
-import Clients from './pages/Clients';
 import Dashboard from './pages/Dashboard';
-import InvoiceSettings from './pages/InvoiceSettings';
-import Invoices from './pages/Invoices';
-import ProjectDetail from './pages/ProjectDetail';
+import Clients from './pages/Clients';
 import Projects from './pages/Projects';
-import TaskStatuses from './pages/TaskStatuses';
+import ProjectDetail from './pages/ProjectDetail';
 import TimeTracking from './pages/TimeTracking';
+import Invoices from './pages/Invoices';
+import TaskStatuses from './pages/TaskStatuses';
+import InvoiceSettings from './pages/InvoiceSettings';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Clients": Clients,
     "Dashboard": Dashboard,
-    "InvoiceSettings": InvoiceSettings,
-    "Invoices": Invoices,
-    "ProjectDetail": ProjectDetail,
+    "Clients": Clients,
     "Projects": Projects,
-    "TaskStatuses": TaskStatuses,
+    "ProjectDetail": ProjectDetail,
     "TimeTracking": TimeTracking,
+    "Invoices": Invoices,
+    "TaskStatuses": TaskStatuses,
+    "InvoiceSettings": InvoiceSettings,
 }
 
 export const pagesConfig = {
